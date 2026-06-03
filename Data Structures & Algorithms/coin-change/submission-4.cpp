394a9c962e742a0aeca52573b53ed0c7b7bfa85b
Deepanshu <154528986+deepanshusaxena315@users.noreply.github.com>
2026-06-03 12:19:56 +0530

    Add: coin-change - submission-4

---
+++ Data Structures & Algorithms/coin-change/submission-4.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        // vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        // vector<int> dp(amount+1,-1);
+        // int ans = solve(amount,coins,dp);
+        // return ans == INT_MAX ? -1 : ans;
+
+        vector<int> dp(amount+1,amount+1);
+        dp[0] = 0;
+        for(int amt = 1;amt<=amount;amt++){
+            for(auto &coin: coins){
+                if(coin<=amt){
+                    dp[amt] = min(dp[amt],1 + dp[amt-coin]);
+                }
+            }
+        }
+        return dp[amount]>amount? -1: dp[amount];
+    }
+
+    int solve(int amt,vector<int> &coins,vector<int> &dp){
+        int n = coins.size();
+        if(amt==0) return 0;
+        if(amt<0) return INT_MAX;
+        if(dp[amt]!=-1) return dp[amt];
+
+        int res = INT_MAX;
+        for(auto &coin: coins){
+            int take = solve(amt-coin,coins,dp);
+            if(take!=INT_MAX) res = min(res,1 + take);
+        }
+        return dp[amt] = res;
+    }
+
+
+};
