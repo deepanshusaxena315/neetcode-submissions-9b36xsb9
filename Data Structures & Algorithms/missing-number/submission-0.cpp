49b6e7df384e8a1f3c0e66860c7b4e3418dd34a5
Deepanshu <154528986+deepanshusaxena315@users.noreply.github.com>
2026-06-06 12:02:16 +0530

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n  = nums.size();
+        vector<int> hash(n+1,0);
+        for(auto &num: nums){
+            hash[num] = 1;
+        }
+        for(int i=0;i<=n;i++){
+            if(hash[i]==0) return i;
+        }
+        return -1;
+    }
+};
